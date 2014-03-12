ede25bcfa64b2c7eb61d130f024aad2caca2bfd2
Ramon E. Ledezma <ramonlv93@gmail.com>
2014-03-11 22:37:48 -0430

    Commit inicial, header en proceso

---
+++ -/components/php/header.php
@@ -0,0 +1,32 @@
+		<header class="clearfix">
+		    <div class="row">
+		    	<div class="col-lg-3 col-sm-offset-9 col-xs-offset-6 col-xs-6">
+		    		<a href="#"><img src="/images/flag_venezuela.png"></a>
+		    		<a href="#"><img src="/images/flag_us.png"></a>
+		    		<a href="#"><img src="/images/flag_portugal.gif"></a>
+		    	</div> <!-- Banderas -->
+		    </div> <!-- Row -->
+		    <div class="row">
+				<section class="branding col-md-4">
+					<a href="index.php"><img src="/images/logo.gif" alt="Logo"></a>
+				</section> <!-- branding -->
+				<div class="direccion col-md-6 col-md-offset-2">
+				  <ul>
+				  	<li>Direccion: Avenida Romulo Gallegos cruce con Calle Shettino</li>
+				  	<li>Valle de la Pascua Estado Guarico</li>
+				  	<li>Telefonos: 0235 - 3412073 / 0235 3422440</li>
+				  </ul>
+				</div> <!-- Direccion -->
+            </div> <!-- row -->
+			<div class="navbar navbar-default">
+				<ul class="nav nav-pills nav-justified">
+					<li><a href="#">Inicio</a></li>
+					<li><a href="#">Servicios</a></li>
+					<li><a href="#">Habitaciones</a></li>
+					<li><a href="#">Tasca-Restaurant</a></li>
+					<li><a href="#">Reservas</a></li>
+					<li><a href="#">Preguntas</a></li>
+					<li><a href="#">Contacto</a></li>
+				</ul> <!-- nav -->
+			</div> <!-- navbar -->
+		</header>
---
+++ es/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="/images/favicon.png" />
+    <title>Palace Hotel, C.A.</title>
+
+
+    <link href="../-/css/bootstrap.css" rel="stylesheet">
+    <link href="../-/css/styles.css" rel="stylesheet">
+  </head>
+
+  <body id="principal">
+    <div class="container">
+      <?php include "../-/components/php/header.php" ?> <!-- Header -->
+
+      <!-- Carousel + Reserva -->
+      
+
+      <!-- Body -->
+
+
+    </div> <!-- Container -->
+
+    <script src="../-/js/myscripts.js"></script>
+    <script src="../-/js/jquery.js"></script>
+    <script src="../-/js/bootstrap.js"></script>
+  </body>
+</html>
