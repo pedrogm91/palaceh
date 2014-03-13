2156a598b43f7316afad559cf26a7deeb82678c1
Ramon E. Ledezma <ramonlv93@gmail.com>
2014-03-12 19:56:39 -0430

    Se cambio logo por carousel_holder

---
+++ -/components/php/snippet_carousel.php
@@ -0,0 +1,20 @@
+<div id="myCarousel" class="carousel  hidden-sm hidden-xs slide">
+    <ol class="carousel-indicators">
+    	<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    	<li data-target="#myCarousel" data-slide-to="1"></li>
+    	<li data-target="#myCarousel" data-slide-to="2"></li>
+    	<li data-target="#myCarousel" data-slide-to="3"></li>
+    	<li data-target="#myCarousel" data-slide-to="4"></li>
+    </ol>
+	<section class="carousel-inner">
+			<div class="active item"><img src="../images/carousel_holder.gif" alt="Photo of Seattle Skyline"></div>
+			<div class="item"><img src="../images/carousel_holder.gif" alt="Photo of jewelry_workshop"></div>
+			<div class="item"><img src="../images/carousel_holder.gif" alt="Photo of painting_workshop"></div>
+			<div class="item"><img src="../images/carousel_holder.gif" alt="Photo of pottery_workshop"></div>
+			<div class="item"><img src="../images/carousel_holder.gif" alt="Photo of quicksketch_workshop"></div>
+	</section> <!-- Carousel-Inner -->
+
+	<a href="#myCarousel" class="left carousel-control" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+    <a href="#myCarousel" class="right carousel-control" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+
+</div> <!-- myCarousel -->
