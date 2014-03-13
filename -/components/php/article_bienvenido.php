3251738474210eb5353e8e78f8fb815c995e7dbe
Ramon E. Ledezma <ramonlv93@gmail.com>
2014-03-13 00:31:17 -0430

    Ajustada imagen de bienvenidos para mejor ajuste al texto

--- -/components/php/article_bienvenido.php
+++ -/components/php/article_bienvenido.php
@@ -1,8 +1,8 @@
 <article class="bienvenidos">
   <h1>Bienvenidos a Palace Hotel</h1>
 		<article class="media">
-		   <img class="pull-right" src="/images/bienvenido_holder.gif" alt="Bienvenido">
-		   <p class="media-body wraper"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, et amet asperiores vero cupiditate alias
+		   <img id="bienvenido" class="pull-right img-responsive" src="/images/bienvenido_holder.gif" alt="Bienvenido">
+		   <p class="media-body"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, et amet asperiores vero cupiditate alias
 		    sequi placeat. Deleniti, provident, ipsa vel adipisci perferendis quibusdam facere quas quisquam! Ipsum, atque, nulla.
 		    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, porro quidem beatae illo distinctio modi totam possimus autem 
 		    labore ipsa asperiores ipsum expedita tempore laudantium deleniti nemo dignissimos officiis impedit.
