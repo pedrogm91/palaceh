679b6f7c4200676f5677f3c304f18dd53615ab4b
Ramon E. Ledezma <ramonlv93@gmail.com>
2014-03-14 23:36:55 -0430

    Reparados problema de alineacion en article_bienvenido

--- -/components/php/article_bienvenido.php
+++ -/components/php/article_bienvenido.php
@@ -1,8 +1,9 @@
 <section class="bienvenidos">
   <h1>Bienvenidos a Palace Hotel</h1>
 		<article class="media">
+		   <p class="media-body"> 
 		   <img id="bienvenido" class="pull-right img-responsive" src="/images/bienvenido_holder.gif" alt="Bienvenido">
-		   <p class="media-body"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, et amet asperiores vero cupiditate alias
+		    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, et amet asperiores vero cupiditate alias
 		    sequi placeat. Deleniti, provident, ipsa vel adipisci perferendis quibusdam facere quas quisquam! Ipsum, atque, nulla.
 		    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, porro quidem beatae illo distinctio modi totam possimus autem 
 		    labore ipsa asperiores ipsum expedita tempore laudantium deleniti nemo dignissimos officiis impedit.
