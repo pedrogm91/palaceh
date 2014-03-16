b575605c1ef86c97c58b9e06caf8f976bf720b86
Ramon E. Ledezma <ramonlv93@gmail.com>
2014-03-16 00:07:33 -0430

    Reserva ya no colapsa en xs

--- -/components/php/snippet-formulario_reserva.php
+++ -/components/php/snippet-formulario_reserva.php
@@ -3,51 +3,51 @@
  <fieldset>
  <legend><h2>Solicitud de Reserva</h2></legend>
   <div class="form-group">
-    <label for="solicitante" class="col-sm-4 control-label">Solicitante</label>
-    <div class="col-sm-8">
+    <label for="solicitante" class="col-xs-4 col-sm-4 control-label">Solicitante</label>
+    <div class="col-xs-8 col-sm-8">
       <input type="text" class="form-control" name="solicitante" id="solicitante" placeholder="Ej: Palace Hotel, C.A.">
     </div>
   </div>
   <div class="form-group">
-      <label for="email" class="col-sm-4 control-label">E-mail</label>
-    <div class="col-sm-8">
+      <label for="email" class="col-xs-4 col-sm-4 control-label">E-mail</label>
+    <div class="col-xs-8 col-sm-8">
       <input type="text" class="form-control" name="email" id="email" placeholder="Email">
     </div>
   </div>
   <div class="form-group">
-      <label for="telefono" class="col-sm-4 control-label">Telefono</label>
-    <div class="col-sm-8">
+      <label for="telefono" class="col-xs-4 col-sm-4 control-label">Telefono</label>
+    <div class="col-xs-8 col-sm-8">
       <input type="text" class="form-control" name="telefono" id="telefono" placeholder="Telefono">
     </div>
   </div>
 
   <div class="form-group">
-      <label for="identificacion" class="col-sm-4 control-label">Cedula / R.I.F.</label>
-    <div class="col-sm-8">
+      <label for="identificacion" class="col-xs-4 col-sm-4 control-label">Cedula / R.I.F.</label>
+    <div class="col-xs-8 col-sm-8">
       <input type="text" class="form-control" name="identificacion" id="identificacion" placeholder="ej: V-20.955.549">
     </div>
   </div>
   <div class="form-group">
-      <label for="checkin" class="col-sm-4 control-label">Check IN</label>
-    <div class="col-sm-8">
+      <label for="checkin" class="col-xs-4 col-sm-4 control-label">Check IN</label>
+    <div class="col-xs-8 col-sm-8">
       <input type="date" class="form-control" name="checkin" id="checkin" placeholder="Ej: 24/12/2013">
     </div>
   </div>
   <div class="form-group">
-      <label for="checkout" class="col-sm-4 control-label">Check OUT</label>
-    <div class="col-sm-8">
+      <label for="checkout" class="col-xs-4 col-sm-4 control-label">Check OUT</label>
+    <div class="col-xs-8 col-sm-8">
       <input type="date" class="form-control" name="checkout" id="checkout" placeholder="Ej: 25/12/2013">
     </div>
   </div>
   <div class="form-group">
-      <label for="pax" class="col-sm-4 control-label">PAX</label>
-    <div class="col-sm-8">
+      <label for="pax" class="col-xs-4 col-sm-4 control-label">PAX</label>
+    <div class="col-xs-8 col-sm-8">
       <input type="text" class="form-control" name="pax" id="pax" placeholder="ej: 3">
     </div>
   </div>
   <div class="form-group">
-      <label for="Comentarios" class="col-sm-4 control-label">Comentarios</label>
-    <div class="col-sm-8">
+      <label for="Comentarios" class="col-xs-4 col-sm-4 control-label">Comentarios</label>
+    <div class="col-xs-8 col-sm-8">
       <textarea class="form-control" name="Comentarios" id="Comentarios" placeholder="comentarios"></textarea>
     </div>
   </div>
