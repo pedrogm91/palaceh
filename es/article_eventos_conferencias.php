bd3efb30f4060259b1e7ced63eacd325dcfc51c4
lagseeing <razondota@gmail.com>
2014-03-18 22:58:35 -0430

    Agregado eventos_conferencias.php

---
+++ es/article_eventos_conferencias.php
@@ -0,0 +1,9 @@
+<section class="event">
+<article>
+<h1>Salon de Eventos y Conferencias</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing
+ elit. Numquam, ad, modi debitis voluptas reiciendis eligendi 
+ mollitia repellendus sed perferendis distinctio consequuntur fugit
+ minus error neque rem harum expedita fugiat vero!</p>
+</article>
+</section>
