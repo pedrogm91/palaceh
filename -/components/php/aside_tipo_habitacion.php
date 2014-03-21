03c3de13541750fb35262517dd61c0c9f3663f47
Ramon E. Ledezma <ramonlv93@gmail.com>
2014-03-20 19:42:33 -0430

    Acordeon de habitaciones casi terminado, agregados iconos, tipos de habitacion, faltan descripciones

--- -/components/php/aside_tipo_habitacion.php
+++ -/components/php/aside_tipo_habitacion.php
@@ -7,7 +7,7 @@
 			<section class="panel panel-default">
 				<div class="panel-heading">
 					<a class="panel-title" data-toggle="collapse" data-parent="#atiposhab" href="#matrimonial">
-						Matrimonial <img class="pull-right" src="/images/icons/bed-32.png"> </a>
+						Matrimonial <img class="mat pull-right" src="/images/icons/bed-32.png"> </a>
 					</div><!-- accordion heading -->
 					<div id="matrimonial" class="panel-collapse collapse in">
 						<section class="panel-body">
@@ -21,9 +21,11 @@
 			<section class="panel panel-default">
 				<div class="panel-heading">
 					<a class="panel-title" data-toggle="collapse" data-parent="#atiposhab" href="#matind">
-						Matrimonial + Individual <img class="pull-right" src="/images/icons/bed-32.png"> </a>
+						Matrimonial + Individual <img class="ind pull-right" src="/images/icons/singlebed32.png"> 
+						                         <img class="mat pull-right" src="/images/icons/bed-32.png">
+				    </a>
 					</div><!-- accordion heading -->
-					<div id="matrimonial" class="panel-collapse collapse in">
+					<div id="matind" class="panel-collapse collapse">
 						<section class="panel-body">
 							<p>Descripcion</p>
 							<p><a href="habitaciones.php#matrimonial" class="btn btn-danger btn-small">Mas informacion</a></p>
@@ -31,6 +33,58 @@
 					</div><!-- accordion-body -->
 				</section><!-- accordion-group -->
 
+			<!-- Doble Matrimonial ------------------------------>	
+			<section class="panel panel-default">
+				<div class="panel-heading">
+					<a class="panel-title" data-toggle="collapse" data-parent="#atiposhab" href="#doblemat">
+						Doble Matrimonial <img class="mat pull-right" src="/images/icons/bed-32.png"> 
+						                         <img class="mat pull-right" src="/images/icons/bed-32.png">
+				    </a>
+					</div><!-- accordion heading -->
+					<div id="doblemat" class="panel-collapse collapse">
+						<section class="panel-body">
+							<p>Descripcion</p>
+							<p><a href="habitaciones.php#matrimonial" class="btn btn-danger btn-small">Mas informacion</a></p>
+						</section><!-- accordion-inner -->
+					</div><!-- accordion-body -->
+				</section><!-- accordion-group -->
+
+                <!-- Suite Junior ------------------------------>
+				<section class="panel panel-default">
+				<div class="panel-heading">
+					<a class="panel-title" data-toggle="collapse" data-parent="#atiposhab" href="#sjunior">
+						Suite Junior             <img class="ind pull-right" src="/images/icons/singlebed32.png">
+						                         <img class="mat pull-right" src="/images/icons/bed-32.png">
+						                         <img class="mat pull-right" src="/images/icons/sofa-32.png">
+						                         <img class="mat pull-right" src="/images/icons/fridge-32.png">						                         
+				    </a>
+					</div><!-- accordion heading -->
+					<div id="sjunior" class="panel-collapse collapse">
+						<section class="panel-body">
+							<p>Descripcion</p>
+							<p><a href="habitaciones.php#matrimonial" class="btn btn-danger btn-small">Mas informacion</a></p>
+						</section><!-- accordion-inner -->
+					</div><!-- accordion-body -->
+				</section><!-- accordion-group -->
+
+				<!-- Suite Palace Familiar ------------------------------>
+				<section class="panel panel-default">
+				<div class="panel-heading">
+					<a class="panel-title" data-toggle="collapse" data-parent="#atiposhab" href="#spf">
+						Suite Palace Familiar    <img class="mat pull-right" src="/images/icons/bed-32.png">
+						                         <img class="mat pull-right" src="/images/icons/bed-32.png">
+						                         <img class="mat pull-right" src="/images/icons/sofa-32.png">
+						                         <img class="mat pull-right" src="/images/icons/fridge-32.png">
+						                         <img class="mat pull-right" src="/images/icons/table-32.png">						                         
+				    </a>
+					</div><!-- accordion heading -->
+					<div id="spf" class="panel-collapse collapse">
+						<section class="panel-body">
+							<p>Descripcion</p>
+							<p><a href="habitaciones.php#matrimonial" class="btn btn-danger btn-small">Mas informacion</a></p>
+						</section><!-- accordion-inner -->
+					</div><!-- accordion-body -->
+				</section><!-- accordion-group -->
 
 
 			</div><!-- Habs accordion -->
