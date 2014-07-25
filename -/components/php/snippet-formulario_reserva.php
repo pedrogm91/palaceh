eb8e029f370274a002bd556ed0b86c6ec3cb0408
Ramon E. Ledezma <ramonlv93@gmail.com>
2014-07-25 16:37:45 -0430

    empezado el modal de confirmar reserva

--- -/components/php/snippet-formulario_reserva.php
+++ -/components/php/snippet-formulario_reserva.php
@@ -1,107 +1,155 @@
 <section class="formreserva">
   <form id="formReserva" class="form-horizontal" method="post" role="form" action="../-/components/php/reservar.php"><fieldset id="camposForm">
-      <legend><h2>Solicitud de Reserva</h2></legend>
 
-      <div class="form-group">
-        <label for="solicitante" class="col-xs-4 control-label">Es usuario Registrado?</label>
-        <div class="form-inline col-xs-8">
-          <input type="radio" class="" name="registrado" id="siRegistrado" value="1">Si&nbsp
-          <input type="radio" checked class="" name="registrado" id="noRegistrado" value="0">No&nbsp&nbsp
-          <span id="ocultoRes" class="ocultoRes">Usuario:&nbsp<input type="text" name="identificacion" size="8" class="form-control">&nbspClave:&nbsp<input type="password" name="clave" size="8" class="form-control"></span>
-        </div>
-      </div> <!-- Usuario Registrado -->
-
-      <div class="form-group">
-        <label for="solicitante" class="col-xs-4 col-sm-4 control-label">Solicitante</label>
-        <div class="col-xs-8 col-sm-8">
-          <input type="text" class="form-control" name="solicitante" id="solicitante" placeholder="Ej: Palace Hotel, C.A.">
-        </div>
-      </div> <!-- Solicitante -->
-
-      <div class="form-group">
-        <label for="email" class="col-xs-4 col-sm-4 control-label">E-mail</label>
-        <div class="col-xs-8 col-sm-8">
-          <input type="text" class="form-control" name="email" id="email" placeholder="Email">  
-        </div>
-      </div> <!-- Email -->
-
-      <div class="form-group">
-        <label for="telefono" class="col-xs-4 col-sm-4 control-label">Telefono</label>
-        <div class="col-xs-8 col-sm-8">
-          <input type="text" class="form-control" name="telefono" id="telefono" placeholder="Telefono">
-        </div>
-      </div> <!-- Telefono -->
-
-      <div class="form-group">
-        <label for="cedrif" class="col-xs-4 col-sm-4 control-label">Cedula / R.I.F.</label>
-        <div class="col-xs-8 col-sm-8">
-           <div class="input-group">
-                <div class="input-group-btn">
-                  <button type="button" id="tipoCed" class="btn btn-default 
-                     dropdown-toggle" data-toggle="dropdown">V <span class="caret"></span>
-                  </button>
-                  <ul class="dropdown-menu">
-                     <li><a id="Ven">V</a></li>
-                     <li><a id="Ex">E</a></li>
-                     <li><a id="Jud">J</a></li>
-                     <li><a id="Gob">G</a></li>
-                  </ul>
-               </div><!-- /btn-group -->
-              <input type="hidden" value="V" id="tipoCedu" name="tipoCedu">
-              <input type="text" class="form-control" name="cedrif" id="cedrif" placeholder="ej: 305241945">
-           </div>
-        </div>
-      </div> <!-- Indetificacion -->
-
-      <div class="form-group habitas">
-        <span class="form-inline col-xs-4">
-          <label for="tipohabitacion[]">1)</label><select name="tipohabitacion[]" id="tipohabitacion1" class="form-control listaha">
-          <option value="Matrimonial">Matrimonial</option>
-          <option value="Matrimonial1">Matrimonial + Individual</option>
-          <option value="3ind">3 Camas Individuales</option>
-          <option value="dobmat">Doble Matrimonial</option>
-          <option value="4ind">Cuatro Camas Individuales</option>
-          <option value="suitej">Suite Junior</option>
-          <option value="suitep">Suite Palace</option>
-          <option value="suitepf">Suite Palace Familiar</option>
-        </select>
-      </span>
-      <span class="form-inline col-xs-8 checkinout">
-        <label for="checkin[]" class="col-sm-1 col-xs-12 control-label">Check-IN:</label>
-        <span class="col-sm-4 col-xs-12">
-          <input placeholder="Ej: 01/01/2014" type="date" style="" class="form-control" name="checkin[]">
-        </span>
-          <label for="checkout[]" class="col-sm-1 asdf col-xs-12 control-label">Check-OUT:</label>
-          <span class="col-sm-4 col-xs-12 chek">
-            <input placeholder="Ej: 01/01/2014" type="date" style="" class="form-control" name="checkout[]">
-          </span>
-            <span></span>
-          </span> <!-- Checkin and Out -->
-        </div><div id="habHolder"></div><span id="punteroHabs"></span>
-        <span><img id="bMas" style="width:30px; height:30px; cursor:pointer;" src="/images/boton_mas.png"></span>
-        <span><img id="bMenos" style="width:30px; height:30px; cursor:pointer;" src="/images/boton_menos.png"></span>
-
-        <div class="form-group">
-          <label for="huesped[]" class="col-xs-4 col-sm-4 control-label">Huesped(es) Habitacion 1:</label>
-          <div class="col-xs-8 col-sm-8">
-            <input type="text" class="form-control" name="huesped[]" id="huesped1" placeholder="ej: Juan Alfonzo y Maria Arteaga">
+    <div class="modal fade bs-example-modal-lg" id="myModal">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+            <h4 class="modal-title">Confirmacion de Reserva</h4>
           </div>
-        </div> <!-- Huesped(es) -->
-        <div id="huesHolder"></div><span id="punteroHues"></span>
-      
-      <div class="form-group">
-        <label for="Comentarios" class="col-xs-4 col-sm-4 control-label">Comentarios</label>
-        <div class="col-xs-8 col-sm-8">
-          <textarea class="form-control" name="Comentarios" id="Comentarios" placeholder="comentarios"></textarea>
-        </div>
-      </div> <!-- Comentarios -->
+          <div class="modal-body">
+            <table border="1">
+              <tr><th colspan="7">Datos del Solicitante</th><tr>
+              <tr>
+                <td>Solicitante</td>
+                <td colspan="6">Ramon Ledezma</td>
+              </tr>
+              <tr>
+                <td>E-Mail</td>
+                <td colspan="6">ramonlv93</td>
+              </tr>
+              <tr>
+                <td>Telefono</td>
+                <td colspan="6">04160350957</td>
+              </tr>
+              <tr>
+                <td> Cedula / RIF</td>
+                <td colspan="6">20955549</td>
+              </tr>
+              <tr><th colspan="7">Habitaciones</th><tr>
+              <tr id="hab1">
+                <td>1)Matrimonial</td>
+                <td>Check-IN</td>
+                <td>24/07/2014</td>
+                <td>Check-Out</td>
+                <td>25/07/2014</td>
+                <td>Huesped(es)</td><td>Ramon Ledezma y Daniela Baloa</td>
+              </tr>
+              <tr><th colspan="7">Huespedes</th><tr>
 
-      <div class="form-group">
-        <div class="col-sm-offset-4">
-          <button type="submit" class="btn btn-default">Reservar</button>
-        </div>
-      </div> <!-- Boton -->
-      
-    </fieldset> <!-- Campos -->
-    </form> <!-- Formulario -->
+            </table>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
+    <legend><h2>Solicitud de Reserva</h2></legend>
+
+    <div class="form-group">
+      <label for="solicitante" class="col-xs-4 control-label">Es usuario Registrado?</label>
+      <div class="form-inline col-xs-8">
+        <input type="radio" class="" name="registrado" id="siRegistrado" value="1">Si&nbsp
+        <input type="radio" checked class="" name="registrado" id="noRegistrado" value="0">No&nbsp&nbsp
+        <span id="ocultoRes" class="ocultoRes">Usuario:&nbsp<input type="text" name="identificacion" size="8" class="form-control">&nbspClave:&nbsp<input type="password" name="clave" size="8" class="form-control"></span>
+      </div>
+    </div> <!-- Usuario Registrado -->
+
+    <div class="form-group">
+      <label for="solicitante" class="col-xs-4 col-sm-4 control-label">Solicitante</label>
+      <div class="col-xs-8 col-sm-8">
+        <input type="text" class="form-control" name="solicitante" id="solicitante" placeholder="Ej: Palace Hotel, C.A.">
+      </div>
+    </div> <!-- Solicitante -->
+
+    <div class="form-group">
+      <label for="email" class="col-xs-4 col-sm-4 control-label">E-mail</label>
+      <div class="col-xs-8 col-sm-8">
+        <input type="text" class="form-control" name="email" id="email" placeholder="Email">  
+      </div>
+    </div> <!-- Email -->
+
+    <div class="form-group">
+      <label for="telefono" class="col-xs-4 col-sm-4 control-label">Telefono</label>
+      <div class="col-xs-8 col-sm-8">
+        <input type="text" class="form-control" name="telefono" id="telefono" placeholder="Telefono">
+      </div>
+    </div> <!-- Telefono -->
+
+    <div class="form-group">
+      <label for="cedrif" class="col-xs-4 col-sm-4 control-label">Cedula / R.I.F.</label>
+      <div class="col-xs-8 col-sm-8">
+        <div class="input-group">
+          <div class="input-group-btn">
+            <button type="button" id="tipoCed" class="btn btn-default 
+            dropdown-toggle" data-toggle="dropdown">V <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu">
+           <li><a id="Ven">V</a></li>
+           <li><a id="Ex">E</a></li>
+           <li><a id="Jud">J</a></li>
+           <li><a id="Gob">G</a></li>
+         </ul>
+       </div><!-- /btn-group -->
+       <input type="hidden" value="V" id="tipoCedu" name="tipoCedu">
+       <input type="text" class="form-control" name="cedrif" id="cedrif" placeholder="ej: 305241945">
+     </div>
+   </div>
+ </div> <!-- Indetificacion -->
+
+ <div class="form-group habitas">
+  <span class="form-inline col-xs-4">
+    <label for="tipohabitacion[]">1)</label><select name="tipohabitacion[]" id="tipohabitacion1" class="form-control listaha">
+    <option value="Matrimonial">Matrimonial</option>
+    <option value="Matrimonial1">Matrimonial + Individual</option>
+    <option value="3ind">3 Camas Individuales</option>
+    <option value="dobmat">Doble Matrimonial</option>
+    <option value="4ind">Cuatro Camas Individuales</option>
+    <option value="suitej">Suite Junior</option>
+    <option value="suitep">Suite Palace</option>
+    <option value="suitepf">Suite Palace Familiar</option>
+  </select>
+</span>
+<span class="form-inline col-xs-8 checkinout">
+  <label for="checkin[]" class="col-sm-1 col-xs-12 control-label">Check-IN:</label>
+  <span class="col-sm-4 col-xs-12">
+    <input placeholder="Ej: 01/01/2014" type="date" style="" class="form-control" name="checkin[]">
+  </span>
+  <label for="checkout[]" class="col-sm-1 asdf col-xs-12 control-label">Check-OUT:</label>
+  <span class="col-sm-4 col-xs-12 chek">
+    <input placeholder="Ej: 01/01/2014" type="date" style="" class="form-control" name="checkout[]">
+  </span>
+  <span></span>
+</span> <!-- Checkin and Out -->
+</div><div id="habHolder"></div><span id="punteroHabs"></span>
+<span><img id="bMas" style="width:30px; height:30px; cursor:pointer;" src="/images/boton_mas.png"></span>
+<span><img id="bMenos" style="width:30px; height:30px; cursor:pointer;" src="/images/boton_menos.png"></span>
+
+<div class="form-group">
+  <label for="huesped[]" class="col-xs-4 col-sm-4 control-label">Huesped(es) Habitacion 1:</label>
+  <div class="col-xs-8 col-sm-8">
+    <input type="text" class="form-control" name="huesped[]" id="huesped1" placeholder="ej: Juan Alfonzo y Maria Arteaga">
+  </div>
+</div> <!-- Huesped(es) -->
+<div id="huesHolder"></div><span id="punteroHues"></span>
+
+<div class="form-group">
+  <label for="Comentarios" class="col-xs-4 col-sm-4 control-label">Comentarios</label>
+  <div class="col-xs-8 col-sm-8">
+    <textarea class="form-control" name="Comentarios" id="Comentarios" placeholder="comentarios"></textarea>
+  </div>
+</div> <!-- Comentarios -->
+
+<div class="form-group">
+  <div class="col-sm-offset-4">
+    <button type="button" id="submitModal" class="btn btn-default" data-toggle="modal" data-target="#myModal">Reservar</button>
+  </div>
+</div> <!-- Boton -->
+
+</fieldset> <!-- Campos -->
+</form> <!-- Formulario -->
 </section>
---
+++ es/tabla.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript">
+	
+
+</script>
+</head>
+<body>
+<table>
+<th colspan="2">Datos del Solicitante</th>
+<tr>
+ <td>Solicitante</td>
+ <td>Ramon Ledezma</td>
+</tr>
+<tr>
+	<td>E-Mail</td>
+	<td>ramonlv93</td>
+</tr>
+<tr>
+	<td>Telefono</td>
+	<td>04160350957</td>
+</tr>
+<tr>
+	<td> Cedula / RIF</td>
+    <td>20955549</td>
+</tr>
+<th colspan="3">Habitaciones</th>
+<tr>
+	
+</tr>
+<th colspan="3">Huespedes</th>
+
+</table>
+</body>
+</html>
